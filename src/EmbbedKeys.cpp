364f4bd3754cd81487e1532b7e3d475e6bc22570
frto027 <q602706150@gmail.com>
2026-05-27 13:33:48 +0800

    add asset, with a python script that generate cpp code

--- src/EmbbedKeys.cpp
+++ src/EmbbedKeys.cpp
@@ -1,19 +1,14 @@
 #include "EmbbedKeys.hpp"
 #include "sslocalization/shared/SSL10n.hpp"
+#include "asset.hpp"
 
 void LangCtrl::InitEmbbedKeys(){
-  SSL10n::Database::AddKeyValue("LOQOLIZER_SETTHING_LABEL_LANGUAGE", "Language");
-  SSL10n::Database::AddKeyValue("LOQOLIZER_SETTHING_LABEL_LANGUAGE", "语言设置/Language", SSL10n::L_Simplified_Chinese);
-
-  SSL10n::Database::AddKeyValue("LOQOLIZER_SETTHING_MENU_TITLE", "Mod Language");
-  SSL10n::Database::AddKeyValue("LOQOLIZER_SETTHING_MENU_TITLE", "Mod语言", SSL10n::L_Simplified_Chinese);
-
-  SSL10n::Database::AddKeyValue("LOQOLIZER_SETTHING_APPLY_NOW", "Apply Now");
-  SSL10n::Database::AddKeyValue("LOQOLIZER_SETTHING_APPLY_NOW", "立即应用", SSL10n::L_Simplified_Chinese);
-
-  SSL10n::Database::AddKeyValue("LOQOLIZER_SETTHING_TEXT_KEPT", "Some text will not changed until you restart game.");
-  SSL10n::Database::AddKeyValue("LOQOLIZER_SETTHING_TEXT_KEPT", "部分内容重启游戏生效", SSL10n::L_Simplified_Chinese);
-  
-  SSL10n::Database::AddKeyValue("LOQOLIZER_SETTHING_TEXT_FOLLOW_HINT", "FollowGame need restart game.");
-  SSL10n::Database::AddKeyValue("LOQOLIZER_SETTHING_TEXT_FOLLOW_HINT", "FollowGame需重启游戏生效", SSL10n::L_Simplified_Chinese);
+  SSL10n::Database::PolyglotFormat::AddCSVContent((char*)AssetGenerated::localize_csv, sizeof(AssetGenerated::localize_csv));
+  SSL10n::Database::Helper(SSL10n::L_Simplified_Chinese)
+    .v("LOQOLIZER_SETTHING_LABEL_LANGUAGE", "语言设置/Language")
+    .v("LOQOLIZER_SETTHING_MENU_TITLE", "Mod语言")
+    .v("LOQOLIZER_SETTHING_APPLY_NOW", "立即应用")
+    .v("LOQOLIZER_SETTHING_TEXT_KEPT", "部分内容重启游戏生效")
+    .v("LOQOLIZER_SETTHING_TEXT_FOLLOW_HINT", "FollowGame需重启游戏生效")
+    ;
 }
