e996dad965481802bfe7244548443de7053b1d8e
frto027 <q602706150@gmail.com>
2026-05-27 14:00:40 +0800

    update asset related things

--- src/PolyglotInject.cpp
+++ src/PolyglotInject.cpp
@@ -5,8 +5,15 @@
 #include "BGLib/Polyglot/Localization.hpp"
 #include "UnityEngine/TextAsset.hpp"
 #include <vector>
+#include <variant>
+#include <string>
+#include <string_view>
 
-std::vector<std::string> pending_assets;
+#include "asset.hpp"
+
+std::vector<std::variant<std::string, std::string_view>> pending_assets;
+
+/* this is modified from https://github.com/qe201020335/PolyglotInject */
 
 MAKE_HOOK_MATCH(LocalizationInstallerHook, 
                 &BGLib::Polyglot::LocalizationAsyncInstaller::LoadResourcesBeforeInstall, 
@@ -16,10 +23,16 @@ MAKE_HOOK_MATCH(LocalizationInstallerHook,
                 BGLib::AppFlow::Initialization::AsyncInstaller::IInstallerRegistry* registry) {
     PaperLogger.debug("%s triggered!", name());
     for(auto & asset_text:pending_assets){
-        assets->i___System__Collections__Generic__ICollection_1_T_()->Add(UnityEngine::TextAsset::New_ctor(asset_text));
+        assets->i___System__Collections__Generic__ICollection_1_T_()->Add(UnityEngine::TextAsset::New_ctor(
+            std::holds_alternative<std::string_view>(asset_text)?
+                std::get<std::string_view>(asset_text):
+                std::string_view(std::get<std::string>(asset_text))
+        ));
     }
     self->_mainPolyglotAsset->supportedLanguages->Add(BGLib::Polyglot::Language::Simplified_Chinese);
     LocalizationInstallerHook(self, assets, registry);
+
+    pending_assets.clear();
 }
 
 
@@ -28,4 +41,7 @@ void LangCtrl::PolyglotInjectInit(){
     INSTALL_HOOK(PaperLogger, LocalizationInstallerHook);
 
     //TODO: add local/remote asset file
+    // pending_assets.push_back(std::string_view(
+    //     (const char*)AssetGenerated::polyglot_inject_csv, 
+    //     sizeof(AssetGenerated::polyglot_inject_csv)));
 }
