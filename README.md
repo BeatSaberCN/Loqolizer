0cd4d703fc612d2f735a837bca435d9615211f4c
frto027 <q602706150@gmail.com>
2026-05-29 09:10:05 +0800

    add semver

--- README.md
+++ README.md
@@ -2,10 +2,41 @@
 
 Translate your game and mods
 
+Don't forget clone submoudles if you git clone this repop.
+
+```
+git clone --recursive xxx
+```
+
+or
+
+```
+git submodule init
+git submodule update
+```
+
 Use `qpm s build` to build
 Same goes for `qpm s copy` and `qpm s qmod`
 
 This mod download translate from [ssl10n.csv](https://github.com/frto027/ssl10n.csv/tree/master) at build time.
+
+# Version check
+
+During load time, the Loqolizer find avaliable mods and load avaliable translates for them. Every translate can declare a mod version range. Therefore, modders don't need to worry about translate compatibility issues between mod versions.
+
+We use [Neargye/semver](https://github.com/Neargye/semver) to match the mod version range. Specially, version number `*` for any version. Otherwise, follow the Readme file in that library.
+
+## Embbed Data Version Check
+
+At the mod build time, the mod **only** embbed the every mod's latest version translate, including the version number.
+
+At the runtime, the mod check every mod's id and version. The mismatched translate will be skipped.
+
+## Online Data Version Check
+
+WIP, online data download feature not avaliable.
+
+The mod will iterates every avaliable version data for every mod, and use the first matched data.
 
 ## Credits
 
--- src/EmbbedData.cpp
+++ src/EmbbedData.cpp
@@ -2,6 +2,7 @@
 #include "main.hpp"
 #include "scotland2/shared/loader.hpp"
 #include "sslocalization/shared/SSL10n.hpp"
+#include "semver.hpp"
 
 void LangCtrl::InitEmbbedData(){
     auto loaded_mods = modloader::get_loaded();
@@ -11,9 +12,32 @@ void LangCtrl::InitEmbbedData(){
         if(it == embbedData.end()){
             continue;
         }
-        PaperLogger.info("Load Embbed mod translate for {} (hash {})", it->first, it->second.md5);
+        PaperLogger.info("Load Embbed mod translate for {}({}) (hash {})", it->first, it->second.version, it->second.md5);
 
         //TODO: mod version check
+        const char * versionRange = it->second.version;
+
+        if(versionRange == NULL
+            || (versionRange[0] == '\0')
+            || (versionRange[0] == '*' && versionRange[1] == '\0')
+        ){
+            // load the version
+        }else{
+            auto mod_version = semver::try_parse(mod.info.version);
+            if(mod_version.has_value()){
+                semver::range_set rs;
+                if(semver::parse(versionRange, rs)){
+                    if(!rs.contains(mod_version.value())){
+                        PaperLogger.info("Mod version {} doesn't match embbed data version {}, will not load.", mod.info.version, versionRange);
+                        continue;
+                    }
+                }else{
+                    PaperLogger.info("Version '{}' broken in embbed data. Anyway we will load the translate.", it->second.version);
+                }
+            }else{
+                PaperLogger.info("Mod version parse failed. Anyway we will load the translate.");
+            }
+        }
 
         SSL10n::Database::PolyglotFormat::AddCSVContent(it->second.csv_buffer, it->second.csv_buffer_size);
     }
