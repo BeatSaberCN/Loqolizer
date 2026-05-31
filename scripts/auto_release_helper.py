f19746a6efb4a245e423e0d9b64397efbaacf8c9
frto027 <q602706150@gmail.com>
2026-05-31 15:40:39 +0800

    [no ci] update workflow

--- scripts/auto_release_helper.py
+++ scripts/auto_release_helper.py
@@ -4,7 +4,9 @@ import re
 import json
 import urllib.request
 
-qpm_json_path = Path(__file__).parent.parent / 'qpm.json'#
+qpm_json_path = Path(__file__).parent.parent / 'qpm.json'
+mod_template_path = Path(__file__).parent.parent / 'mod.template.json'
+
 with qpm_json_path.open("r",encoding="utf-8") as f:
     qpm_json = json.load(f)
 version = qpm_json["info"]["version"]
@@ -17,6 +19,21 @@ print(f"Bump release version {version} -> {next_version}")
 has_new_data = True
 remote_json = json.loads(urllib.request.urlopen("https://frto027.github.io/ssl10n.csv/manifest.json?time={time.time()}").read().decode("utf8"))
 # TODO: has_new_data
+
+if has_new_data:
+    print("New data detected, will update qpm.json to prepare the next auto release")
+    qpm_json["info"]["version"] = next_version
+    with qpm_json_path.open("w", encoding="utf-8") as f:
+        json.dump(qpm_json, f)
+
+    print("will update mod.template.json")
+    with mod_template_path.open("r", encoding='utf-8')as f:
+        mod_template = json.load(f)
+    mod_template["version"] = next_version
+    with mod_template_path.open("w", encoding='utf-8') as f:
+        json.dump(mod_template, f)
+    
+    print("qpm.shared.json will not update, please run 'qpm restore'")
 
 import os
 
