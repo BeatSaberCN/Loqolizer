618e27aacf8c95515b54e279b219cdfdaa76b20d
frto027 <q602706150@gmail.com>
2026-06-13 11:00:45 +0800

    dynamic update menu name when game soft restart

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,16 @@
 #include "EmbbedData.hpp"
 #include "HMUI/ViewController.hpp"
 #include "PolyglotInject.hpp"
+#include "SSL10n.hpp"
 #include "beatsaber-hook/shared/utils/typedefs-string.hpp"
 #include "LangCtrl.hpp"
+#include "bsml/shared/BSML/Settings/BSMLSettings.hpp"
 #include "scotland2/shared/modloader.h"
 #include "modconfig.hpp"
 #include "SSL10nGenerated.hpp"
 #include "bsml/shared/BSML.hpp"
+#include "bsml/shared/BSML/Settings/BSMLSettings.hpp"
+#include "bsml/shared/BSML/Settings/SettingsMenu.hpp"
 
 static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
 // Stores the ID and version of our mod, and is sent to
@@ -43,6 +47,17 @@ MOD_EXTERN_FUNC void late_load() noexcept {
   PaperLogger.info("Register BSML Menu");
 
   BSML::Init();
+
+  static SafePtr<BSML::SettingsMenu> menu = BSML::SettingsMenu::Make_new(SSL10nGen::STR::SETTHING_MENU_TITLE(), LangCtrl::DidActivate, false);
+  if (!BSML::BSMLSettings::get_instance()->TryAddSettingsMenu(&*menu)) {
+    PaperLogger.info("Menu init failed");
+  }else{
+    SSL10n::OnLanguageChangeCallback.addCallback([](){
+      menu->text = SSL10nGen::STR::SETTHING_MENU_TITLE();
+    });
+  }
+
+
   BSML::Register::RegisterSettingsMenu(SSL10nGen::STR::SETTHING_MENU_TITLE(),LangCtrl::DidActivate, false);
 
   PaperLogger.info("Init PolyglotInject");
